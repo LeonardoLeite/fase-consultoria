992e6fd50f5ecdbc2bef26b763b15263638406b0
Leonardo Bezerra Leite <lableite@gmail.com>
2024-01-23 16:51:12 -0300

    Update TheFooter.vue
    Minor update of the footer component, focused on the CSS

--- src/components/layouts/TheFooter.vue
+++ src/components/layouts/TheFooter.vue
@@ -3,25 +3,29 @@
   <footer>
     <img src="../../assets/images/horizontallogo.svg" alt="Company logo" />
     <div id="menu">
-      <div id="company">
-        <h3>Empresa</h3>
-        <p>Sobre nós</p>
-        <p>Áreas de atuação</p>
-        <p>Como te ajudamos</p>
-        <p>Quem ajudamos</p>
-      </div>
-      <div id="services">
-        <h3>Serviços</h3>
-        <p>Consultoria de TI</p>
-        <p>Desenv. e gestão de sites</p>
-        <p>Design gráfico</p>
-        <p>Criação e registro de marcas</p>
-      </div>
-      <div id="Social">
-        <h3>Social</h3>
-        <p>GutHub</p>
-        <p>Instagram</p>
-        <p>WhatsApp</p>
+      <div id="menuLeft">
+        <div id="company">
+          <!-- TODO substituir os elementos p por router-links -->
+          <h3>Empresa</h3>
+          <p>Sobre nós</p>
+          <p>Áreas de atuação</p>
+          <p>Como te ajudamos</p>
+          <p>Quem ajudamos</p>
+        </div>
+        <div id="services">
+          <h3>Serviços</h3>
+          <p>Consultoria de TI</p>
+          <p>Desenv. e gestão de sites</p>
+          <p>Design gráfico</p>
+          <p>Criação e registro de marcas</p>
+        </div>
+        <div id="Social">
+          <!-- TODO pintar os links com as cores das redes -->
+          <h3>Social</h3>
+          <p>GutHub</p>
+          <p>Instagram</p>
+          <p>WhatsApp</p>
+        </div>
       </div>
       <div id="letsTalk">
         <h2>Vamos conversar?</h2>
@@ -46,14 +50,34 @@ export default {
 <style scoped>
 footer {
   background-color: var(--main-blue-color);
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
   padding: 2rem 3rem;
 }
 
 img {
   max-height: 40px;
+  align-self: flex-start;
 }
 
 #menu {
   display: flex;
+}
+
+#menuLeft {
+  width: 60%;
+  display: flex;
+  justify-content: center;
+  gap: 4rem;
+}
+
+#letsTalk {
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
 }
 </style>
